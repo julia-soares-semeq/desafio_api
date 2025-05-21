55039b7ecb52f8f5200ccdf15c2845f73f2d6047
julia-soares-semeq <julia.soares@semeq.com>
2025-05-21 13:28:44 -0300

    feat: add structure and logic

--- frontend/front-end-react/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-//create an instance of axios with the base URL
-const instance = axios.create({
-    baseURL: 'http://localhost:8000/'
-});
-
-export default instance;
